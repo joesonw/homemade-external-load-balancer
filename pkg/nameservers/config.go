02226e8509b4aacea3f6ba4fc6660566ece3d078
Qiaosen Huang <joesonw@gmail.com>
2019-03-17 20:56:19 +0800

    remove godaddy

--- pkg/nameservers/config.go
+++ pkg/nameservers/config.go
@@ -3,15 +3,12 @@ package nameservers
 import "fmt"
 
 type Config struct {
-	Dnspod  *DnspodConfig  `yaml:"dnspod"`
-	Godaddy *GodaddyConfig `yaml:"godaddy"`
+	Dnspod *DnspodConfig `yaml:"dnspod"`
 }
 
 func New(cfg *Config) (Interface, error) {
 	if cfg.Dnspod != nil {
 		return NewDnspod(cfg.Dnspod)
-	} else if cfg.Godaddy != nil {
-		return NewGodday(cfg.Godaddy)
 	}
 	return nil, fmt.Errorf("usupported nameserver provider")
 }
